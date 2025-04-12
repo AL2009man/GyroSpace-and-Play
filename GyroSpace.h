423f951c3bfd14260f1467089cbb2e6c9c6b01a7
AL2009man <67606569+AL2009man@users.noreply.github.com>
2025-04-11 22:18:36 -0400

    Updated to version 0.4.1

--- GyroSpace.h
+++ GyroSpace.h
@@ -2,7 +2,7 @@
  * =======================================================================
  *
  * Gyro Space to Play - A plug-and-play Gyro Space Transformer code
- * Version 0.4.0
+ * Version 0.4.1
  *
  * Provides functionality for transforming gyro inputs into Local Space,
  * Player Space, and World Space, while handling sensitivity adjustments
